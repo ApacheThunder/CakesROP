97e63792c81227dbe2bafeb21d97766db4b07bda
ApacheThunder <spenserwhaley@gmail.com>
2015-07-06 12:59:07 -0500

    Added latest changes to CakesROPSpider
    Added changes from official branch of CakesROP.
    
    (specifially the CakesROPSpider part)

--- CakesROPSpider/src/nvram.c
+++ CakesROPSpider/src/nvram.c
@@ -8,41 +8,40 @@
 #include "patches.h"
 #include "ctru.h"
 
-static struct file *const patchf = (void*)0x08F10000;
+#define PAYLOAD_FNAME_LEN 0x20
 
 typedef struct index_s
 {
 	char *desc;
-	uint32_t offset;
+	u32 offset;
 } index_s;
 
 typedef struct patch_s
 {
-	uint32_t size;
-	uint32_t offset;
-	uint8_t patch[];
+	u32 size;
+	u32 offset;
+	u8 patch[];
 } patch_s;
 
 typedef struct patch_set_s
 {
-	uint32_t magic;
-	uint32_t count;
-	uint32_t offset[];
+	u32 magic;
+	u32 count;
+	u32 offset[];
 } patch_set_s;
 
 /* Thanks, desmume */
-u32 calc_CRC16(u32 start, const void *dataptr, int count)
+u32 CRC16(u32 start, const void *dataptr, int count)
 {
 	const u8 *data = dataptr;
-	int i, j;
 	u32 crc = start & 0xffff;
 	const u16 val[8] =
 	{0xC0C1, 0xC181, 0xC301, 0xC601, 0xCC01, 0xD801, 0xF001, 0xA001};
-	for(i = 0; i < count; i++)
+	for(int i = 0; i < count; i++)
 	{
 		crc = crc ^ data[i];
 
-		for(j = 0; j < 8; j++)
+		for(int j = 0; j < 8; j++)
 		{
 			int do_bit = 0;
 
@@ -59,39 +58,67 @@ u32 calc_CRC16(u32 start, const void *dataptr, int count)
 	return crc;
 }
 
-void userSettingsCRC(void *buffer)
+void UserSettingsCRC(void *buffer)
 {
 	u16 *slot = buffer;
-	u16 CRC1 = calc_CRC16(0xFFFF, slot, 0x70);
-	u16 CRC2 = calc_CRC16(0xFFFF, &slot[0x3A], 0x8A);
-	slot[0x39] = CRC1;
-	slot[0x7F] = CRC2;
+	u16 crc1 = CRC16(0xFFFF, slot, 0x70);
+	u16 crc2 = CRC16(0xFFFF, &slot[0x3A], 0x8A);
+	slot[0x39] = crc1;
+	slot[0x7F] = crc2;
 }
 
 // Apply zoogie patches
-int apply(const uint8_t *patchBuf, uint8_t *work, uint32_t sel)
+int ApplyPatch(const u8 *patchBuf, u8 *work, u32 sel)
 {
 	const wchar_t cakes[] = L"YS:/Cakes.dat";
-	uint32_t magic = *(uint32_t *)(patchBuf);
+	u32 magic = *(u32 *)(patchBuf);
 	if(magic != 0x524f5050)
 		return -1;
 
-	uint32_t index_offset = *(uint32_t *)(patchBuf + 4);
+	u32 index_offset = *(u32 *)(patchBuf + 4);
 	index_s *indices = (index_s *)(patchBuf + index_offset);
 
 	patch_set_s *ps = (patch_set_s *)(patchBuf + indices[sel].offset);
 
-	for(uint32_t i = 0; i < ps->count; ++i)
+	for(u32 i = 0; i < ps->count; ++i)
 	{
 		patch_s *patch = (patch_s *)(patchBuf + ps->offset[i]);
 		_memcpy(work + patch->offset, patch->patch, patch->size);
 	}
 
-	compat.app.memcpy(work + 0x11C, cakes, 0x20);
+	u8 *name = work + 0x11C;
+
+	IFILE file;
+	_memset(&file, 0, sizeof(file));
+	if(compat.app.IFile_Open(&file, L"dmc:/ropCustom.txt", FILE_R) == 0)
+	{
+		const u32 maxPathLen = PAYLOAD_FNAME_LEN / 2;
+		const u32 maxFnameLen = maxPathLen - 4;
+
+		u8 *custName = (u8 *)(0x18410000 + 0x200);
+		_memset(custName, 0, PAYLOAD_FNAME_LEN);
+		_memcpy(custName, "YS:/", 4);
+
+		unsigned int read;
+		compat.app.IFile_Read(&file, &read, custName + 4, maxFnameLen);
+		for(u32 i = 0; i < maxPathLen; i++)
+		{
+			name[i * 2 + 1] = 0;
+			if(custName[i] == '\n' || custName[i] == '\r' || custName[i] == 0)
+			{
+				name[i * 2] = 0;
+				break;
+			}
+
+			name[i * 2] = custName[i];
+		}
+	}
+	else
+		compat.app.memcpy(name, cakes, PAYLOAD_FNAME_LEN);
 
 	// Fix CRCs
-	userSettingsCRC(work);
-	userSettingsCRC(work + 0x100);
+	UserSettingsCRC(work);
+	UserSettingsCRC(work + 0x100);
 
 	return 0;
 }
@@ -99,21 +126,20 @@ int apply(const uint8_t *patchBuf, uint8_t *work, uint32_t sel)
 Result DumpNVRAM(Handle CFGNOR_handle)
 {
 	Result ret = 0;
-	u32 pos = 0;
 	// chunk size 0x100 taken from ctrulib
 	// no idea why it's so low
 	u32 chunksize = 0x100;
 	u32 size = 128 * 1024;
 	u32 *buf = (u32 *) 0x18410000;
-	int *fileop_len = (void *) 0x08F01000;
 
 	/* Mystery value 1. 3dbrew says it's "usually" 1. */
 	if((ret = CFGNOR_Initialize(CFGNOR_handle, 1)) == 0)
 	{
-		patchf->pos = 0;
-		compat.app.IFile_Open(patchf, L"dmc:/nvram.bin", FILE_W);
+		IFILE file;
+		_memset(&file, 0, sizeof(file));
+		compat.app.IFile_Open(&file, L"dmc:/nvram.bin", FILE_W);
 
-		for(pos = 0; pos < size; pos += chunksize)
+		for(u32 pos = 0; pos < size; pos += chunksize)
 		{
 			if(size - pos < chunksize)
 				chunksize = size - pos;
@@ -121,7 +147,9 @@ Result DumpNVRAM(Handle CFGNOR_handle)
 			ret = CFGNOR_ReadData(CFGNOR_handle, pos, buf, chunksize);
 			if(ret != 0)
 				return ret;
-			compat.app.IFile_Write(patchf, fileop_len, buf, chunksize, 1);
+
+			unsigned int written;
+			compat.app.IFile_Write(&file, &written, buf, chunksize, 1);
 		}
 		compat.app.svcSleepThread(0x400000LL);
 	}
@@ -133,7 +161,6 @@ Result PatchNVRAM(Handle CFGNOR_handle)
 {
 	Result ret = 0;
 	u8 *buf = (u8 *) 0x18410000;
-	u8 *patchBuf = buf + 0x200;
 
 	// Mystery value 1. 3dbrew says it's "usually" 1.
 	if((ret = CFGNOR_Initialize(CFGNOR_handle, 1)) == 0)
@@ -147,7 +174,7 @@ Result PatchNVRAM(Handle CFGNOR_handle)
 
 		if(ret == 0)
 		{
-			if(apply(rawData, buf, compat.patch_sel))
+			if(ApplyPatch(rawData, buf, compat.patch_sel))
 				return -1;
 
 			// ctrulib doesn't read/write more than 0x100 at a time. Better
